bbc98e616da1bc0a7f8489481a3238fd757b3ca6
diwanumang <diwan.umang@gmail.com>
2024-04-06 23:14:44 -0400

    Wrote code for math operations and displayed result and clicked buttons

--- app.js
+++ app.js
@@ -1,9 +1,33 @@
 /*-------------------------------- Constants --------------------------------*/
+const buttons = document.querySelectorAll(".button");
+const displayedNums = document.querySelector(".display");
 
 /*-------------------------------- Variables --------------------------------*/
-
+let calculate = "";
 /*------------------------ Cached Element References ------------------------*/
+const selectedNums = document.createElement("p");
 
 /*----------------------------- Event Listeners -----------------------------*/
+buttons.forEach(button => {
+    button.addEventListener("click", (event) => {
+        selectedNums.innerText = event.target.innerText;
+        displayedNums.append(selectedNums);
+        const selection = event.target.innerText;
+
+        if (selection === "=") {
+            calculate = calcResult(calculate);
+            displayedNums.innerText = calculate;
+        } else if(selection === "C") {
+            calculate = "";
+            displayedNums.innerText = null;
+        } else {
+            calculate += selection;
+        }
+    });
+});
 
 /*-------------------------------- Functions --------------------------------*/
+const calcResult = (input) => {
+    let result = eval(input);
+    return result;
+}
